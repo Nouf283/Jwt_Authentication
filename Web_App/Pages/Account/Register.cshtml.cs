42c3afc5acda42aa484db3cbe59c62d25b57a1e0
Nouf283 <noufawal0311@gmail.com>
2023-02-15 22:40:13 +0600

    User Registration

---
+++ Web_App/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web_App.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress(ErrorMessage ="Invalid Email Addrss.")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(dataType:DataType.Password)]
+        public string Password { get; set; }
+    }
+}
