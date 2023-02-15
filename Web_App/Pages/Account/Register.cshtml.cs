449e64a846cf429838d49946078281c9b082b259
Nouf283 <noufawal0311@gmail.com>
2023-02-16 00:37:05 +0600

    User Registration Form Complete

--- Web_App/Pages/Account/Register.cshtml.cs
+++ Web_App/Pages/Account/Register.cshtml.cs
@@ -1,16 +1,53 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 
 namespace Web_App.Pages.Account
 {
     public class RegisterModel : PageModel
     {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public RegisterModel(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
         [BindProperty]
         public RegisterViewModel RegisterViewModel { get; set; }
         public void OnGet()
         {
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+            // validating Email Address (optional)
+
+            //Create the user
+            var user = new IdentityUser
+            {
+                Email = RegisterViewModel.Email,
+                UserName = RegisterViewModel.Email
+            };
+
+            var result = await this.userManager.CreateAsync(user, RegisterViewModel.Password);
+            
+            if (result.Succeeded)
+            {
+                return RedirectToPage("/Account/Login");
+            }
+            else
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("Register", error.Description);
+                }
+                return Page();
+            }
+        }
+
     }
 
     public class RegisterViewModel
