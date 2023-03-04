5758d075575245c440caaa6860aad91414b3917a
Nouf283 <noufawal0311@gmail.com>
2023-03-04 21:23:29 +0600

    Collecting more user info with claims

--- Web_App/Pages/Account/Register.cshtml.cs
+++ Web_App/Pages/Account/Register.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Net.Mail;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Web_App.Data;
 using Web_App.Services;
@@ -37,14 +38,19 @@ namespace Web_App.Pages.Account
             {
                 Email = RegisterViewModel.Email,
                 UserName = RegisterViewModel.Email,
-                Department=RegisterViewModel.Department,
-                Position=RegisterViewModel.Position
+                //Department=RegisterViewModel.Department,
+                //Position=RegisterViewModel.Position
             };
+            var claimDepartment = new Claim("Department", RegisterViewModel.Department);
+            var claimPosition = new Claim("Position", RegisterViewModel.Position);
 
             var result = await this.userManager.CreateAsync(user, RegisterViewModel.Password);
 
             if (result.Succeeded)
             {
+                await this.userManager.AddClaimAsync(user, claimDepartment);
+                await this.userManager.AddClaimAsync(user, claimPosition);
+
                 var confirmationToken = await this.userManager.GenerateEmailConfirmationTokenAsync(user);
                 var confirmationLink = Url.PageLink(pageName: "/Account/ConfirmEmail",
                      values: new { userId = user.Id, token = confirmationToken });
